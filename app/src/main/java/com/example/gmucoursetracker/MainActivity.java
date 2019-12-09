e5cd8ce8c99e6c99bdee81e33d8ab217410b572e
Francisco Castro <fjcastrocs@gmail.com>
2019-12-09 09:54:23 -0500

    Better loggin

--- app/src/main/java/com/example/gmucoursetracker/MainActivity.java
+++ app/src/main/java/com/example/gmucoursetracker/MainActivity.java
@@ -14,6 +14,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ExpandableListView;
 import android.widget.ExpandableListView.OnChildClickListener;
@@ -25,7 +26,7 @@ import org.json.JSONObject;
 import android.app.AlertDialog;
 
 public class MainActivity extends Activity {
-    private static final String TAG = "MainActivity";
+    private static final String TAG = "+++++++++ MAIN ACTIVITY +++++++++++";
     public  Databasehelper dbHelper=null;
     public  SQLiteDatabase db=null;
     ExpandableListAdapter listAdapter;
@@ -41,12 +42,11 @@ public class MainActivity extends Activity {
     private boolean isSectionTrackerRunning() {
         ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
         for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-            if (SectionTracker.class.equals(service.service.getClassName())) {
-                System.out.println("Service Tracker is already running");
+            if (SectionTracker.class.getName().equals(service.service.getClassName())){
+                Log.i(TAG, "Section Tracker service is already running");
                 return true;
             }
         }
-        System.out.println("Service Tracker is starting...");
         return false;
     }
 
--- app/src/main/java/com/example/gmucoursetracker/ServiceRestartBroadcastReceiver.java
+++ app/src/main/java/com/example/gmucoursetracker/ServiceRestartBroadcastReceiver.java
@@ -3,15 +3,20 @@ package com.example.gmucoursetracker;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 /**
  * Used to restart the Section Tracker Service
  */
 public class ServiceRestartBroadcastReceiver extends BroadcastReceiver {
+    private static final String TAG = "+++++++++ BROADCAST RECEIVER ++++++";
+
+
     @Override
     public void onReceive(Context context, Intent intent) {
         intent = new Intent(context, SectionTracker.class);
         intent.putExtra("start", true);
+        Log.e(TAG, "Restarting Section Tracker Service");
         context.startService(intent);
     }
 }
