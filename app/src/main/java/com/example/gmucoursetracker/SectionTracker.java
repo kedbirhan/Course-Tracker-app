36cae498243525f6f7434612c88d4e8f29ef2ae6
Francisco Castro <fjcastrocs@gmail.com>
2019-12-10 08:51:51 -0500

    all done2

--- app/src/main/java/com/example/gmucoursetracker/SectionTracker.java
+++ app/src/main/java/com/example/gmucoursetracker/SectionTracker.java
@@ -32,7 +32,7 @@ public class SectionTracker extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        // service is starting, restart all section trackers
+        // service is starting, restart all section trackersjkdjndsjn
         if(intent.getBooleanExtra("start", false)){
             Log.i(TAG, "Started");
             databasehelper = new Databasehelper(this);
